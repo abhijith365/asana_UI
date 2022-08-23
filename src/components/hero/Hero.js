b7a1a749a77e76d50d2c44bf017afda102177662
abijith365 <abhijithelampal005@gmail.com>
2022-08-23 14:20:54 +0530

    code updated

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -5,7 +5,7 @@ export const Hero = () => {
 
         <div className="w-98 pb-1/2 flex justify-center items-center overflow-clip bg-purple-200">
             {Array(3).fill(null).map(() => (
-                <img src={img1} className=" bg-center hero-img pt-10 px-10" alt="img" />
+                <img src={img1} className="bg-center hero-img pt-10 px-10" alt="img" />
 
             ))}
 
